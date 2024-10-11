94b41944b2fdc311acc8754c63fb2250345772ed
ideftbuild <a.akanSWE@gamil.com>
2024-10-11 13:57:33 +0100

    feat: Add `floor` function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module: 2-floor
+"""
+
+
+def floor(n: float) -> int:
+    """Returns the largest integer less than or equal to the given float."""
+    from math import floor
+    return floor(n)
