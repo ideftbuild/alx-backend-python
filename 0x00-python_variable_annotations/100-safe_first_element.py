804305a458fb425609f8716bb561bb56edb29230
ideftbuild <a.akanSWE@gamil.com>
2024-10-14 04:10:04 +0100

    refactor: change `NoneType` to `None`

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """Module: 100-safe_first_element"""
-from types import NoneType
 from typing import Sequence, Any, Union
 
 
-def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """Returns the first element of a sequence otherwise None."""
     if lst:
         return lst[0]
