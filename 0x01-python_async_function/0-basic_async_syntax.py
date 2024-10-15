aa01b1a37eea3fab94455c707e6a5c1963289b85
ideftbuild <a.akanSWE@gamil.com>
2024-10-15 18:48:47 +0100

    fix: pycodestyle errors and add documentation

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,11 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
+    """Asynchronous coroutine that takes in an integer argument and
+    waits for a random delay between 0 and max_delay
+    :returns the random delay.
+    """
     sleep_time: float = uniform(0, max_delay)
     await asyncio.sleep(sleep_time)
     return sleep_time
