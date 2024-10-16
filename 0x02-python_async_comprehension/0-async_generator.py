a37aae13678f9fec051a544070464ebdd0d07845
ideftbuild <a.akanSWE@gamil.com>
2024-10-16 05:20:03 +0100

    refactor: use `Generator` for type annotations

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,11 +2,13 @@
 """Module: 0-async_generator"""
 import asyncio
 from random import uniform
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """Async generator that yields random numbers"""
+async def async_generator() -> Generator[float, None, None]:
+    """Asynchronous generator that yields random numbers between 0 and 10
+    after a second delay for a total of 10 iterations
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
